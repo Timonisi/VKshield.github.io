08e98103adc5b1b1957834e25a2b67bbb9a7c1b1
Timonisi <153761386+Timonisi@users.noreply.github.com>
2025-03-28 18:52:08 +0300

    Update server.py

--- server.py
+++ server.py
@@ -139,5 +139,3 @@ def analyze_user():
         print(f"Ошибка: {e}")
         return jsonify({"error": str(e)}), 500
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
